eaac8057008e9bab104921d4e09b8d391d2c8998
clivejj <cjohnston1@stuy.edu>
2017-10-17 21:01:11 -0400

    finished assignment

--- stu_mean.py
+++ stu_mean.py
@@ -15,7 +15,9 @@ for row in pleasePrint:
 	row = str(row)
 	row = row[2:row.find(")")]
 	listOfRows.append(row)
-	print row
+	#print row
+        
+#print listOfRows
 	
 #print listOfRows
 
@@ -52,6 +54,24 @@ for key in sumDict:
 #display each students name,id, and average
 for key in averageDict:
 	print key + " |average=" + str(averageDict[key]) + " | id=" + str(idDict[key])
+        
+c.execute("CREATE TABLE peeps_avg (name TEXT, id INTEGER, average INTEGER)")
+for key in averageDict:
+        c.execute("INSERT INTO peeps_avg VALUES (" + '"' + key + '", '  + str(idDict[key]) + ", " + str(averageDict[key]) + ")")
+
+def updateAverage(name, newAv):
+        c.execute("UPDATE peeps_avg SET average = " + str(newAv) + " WHERE name = " + '"' + name + '"')
+        
+updateAverage("kruder", 100)
+updateAverage("digweed", 90)
+
+def addCourse(code, mark, id):
+        c.execute("INSERT INTO courses VALUES (" + '"' + code + '"' + "," + str(mark) + "," + str(id) + ")")
+
+for num in range(1, 10):    
+        addCourse("maths", 80 + num, num)
+        addCourse("introCS", 80 - num, num)
+
 	
 
 
